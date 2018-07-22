bbe137a2d1bff1d719bcb50debcdd44fabffd8a7
Philipp Matthes <github@philippmatth.es>
2018-07-22 12:44:59 +0200

    Update infiny.js

--- infiny.js
+++ infiny.js
@@ -2,35 +2,48 @@ var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 
-contacts = [];
-chat_div = [];
-
 function triggerMouseEvent(node, eventType) {
     var event = document.createEvent('MouseEvents');
     event.initEvent(eventType, true, true);
     node.dispatchEvent(event);
 }
 
-function getChatname(){
-    $("#side > div > div > div").find("._25Ooe > ._1wjpf").each(function(){
-        console.log($(this).text())
-        contacts.push($(this).text());
-        chat_div.push($(this));
-    })
-    
-     console.log($("#pane-side > div > div > div"))
+function getQRCodeBase64() {
+	return $("#app > div > div > div._2NbD3 > div > div.XSdna > div > img").attr("src");
 }
 
-function selectContact(name){
-    getChatname()
-    for (i = 0; i < contacts.length; i++){
-        if (name.toUpperCase() === contacts[i].toUpperCase()){
-            console.log(name.toUpperCase() + " match with "+contacts[i].toUpperCase())
-             triggerMouseEvent(chat_div[i][0],"mousedown")
-        }else{
-            console.log(name.toUpperCase() + " mismatch with "+contacts[i].toUpperCase())
-        }
-    }
+function getGroupChatDivs() {
+    return $("#side > div > div > div").find("._25Ooe > ._1wjpf");
+}
+
+function getContactChatDivs() {
+	return $("#side > div > div > div").find("._25Ooe > ._3TEwt > ._1wjpf");
+}
+
+function getAllChatDivs() {
+	return $.merge(getGroupChatDivs(), getContactChatDivs());
+}
+
+function chatName(chatDiv) {
+	return $(chatDiv).text();
+}
+
+function selectChat(chatDiv){
+	triggerMouseEvent(chatDiv, "mousedown");
+}
+
+function getMessageDivs() {
+	return $("#main > div._3zJZ2 > div > div > div._9tCEa > div > div");
+}
+
+function getMessageFromDiv(messageDiv) {
+	let caption = $(messageDiv).find("div.KYpDv._3zdTI._1tq8Y.copyable-text > div > div._1RiwZ > div > span > a").text();
+	let message = $(messageDiv).find("div.copyable-text > div > span").text()
+	return caption + message;
+}
+
+function getSenderFromDiv(messageDiv) {
+	return $(messageDiv).find("div._111ze > span").text();
 }
 
 function sendMessage(text){
